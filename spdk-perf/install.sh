4089a1767c6c42789a2d24e330e8265165ff0d5d
Jianshen Liu <jliu120@ucsc.edu>
2017-10-27 21:20:53 -0700

    Support extracting both the throughput and IOPS from the result profile

--- spdk-perf/install.sh
+++ spdk-perf/install.sh
@@ -13,9 +13,9 @@ else
 
     cd ~
     cat <<-EOF > spdk-perf
-#!/bin/sh
-./spdk/examples/nvme/perf/perf -r 'trtype:PCIe traddr:$pcieaddr' \$@ > \$LOG_FILE 2>&1
-sed -i 's/\(Total \+: \+\)[0-9\.]\+ \+/\1/' \$LOG_FILE
+#!/bin/bash
+./spdk/examples/nvme/perf/perf -r 'trtype:PCIe traddr:$pcieaddr' -s 4096 -t 10 \${@:2} > \$LOG_FILE 2>&1
+sed -i 's/Total \+: \+[0-9\.]\+/\0 \|/' \$LOG_FILE
 echo \$? > ~/test-exit-status
 EOF
     chmod +x spdk-perf
